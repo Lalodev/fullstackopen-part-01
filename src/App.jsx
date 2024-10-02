36693dc3cec414c2ec80a9437cef0af6b5dc271d
lalodev <laloing87@gmail.com>
2024-10-01 23:35:12 -0600

    1.12*: anecdotes, paso 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,53 @@
 import { useState } from "react";
 import { Button } from "./components/Button";
 import { Statistics } from "./components/Statistics";
+import { Anecdotes } from "./components/Anecdotes";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+  // const [total, setTotal] = useState(0);
+  // const [average, setAverage] = useState(0);
+  // const [positive, setPositive] = useState(0);
 
-  const handleClickGood = () => {
-    const newFeedback = good + 1;
-    setGood(newFeedback);
-    const newTotal = newFeedback + neutral + bad;
-    setTotal(newTotal);
-    const newAverage = (1 * newFeedback + 0 * neutral + -1 * bad) / newTotal;
-    setAverage(newAverage);
-    const newPositive = (newFeedback * 100) / newTotal;
-    setPositive(newPositive);
-  };
+  // const handleClickGood = () => {
+  //   const newFeedback = good + 1;
+  //   setGood(newFeedback);
+  //   const newTotal = newFeedback + neutral + bad;
+  //   setTotal(newTotal);
+  //   const newAverage = (1 * newFeedback + 0 * neutral + -1 * bad) / newTotal;
+  //   setAverage(newAverage);
+  //   const newPositive = (newFeedback * 100) / newTotal;
+  //   setPositive(newPositive);
+  // };
 
-  const handleClickNeutral = () => {
-    const newFeedback = neutral + 1;
-    setNeutral(newFeedback);
-    const newTotal = good + newFeedback + bad;
-    setTotal(newTotal);
-    const newAverage = (1 * good + 0 * newFeedback + -1 * bad) / newTotal;
-    setAverage(newAverage);
-    const newPositive = (good * 100) / newTotal;
-    setPositive(newPositive);
-  };
+  // const handleClickNeutral = () => {
+  //   const newFeedback = neutral + 1;
+  //   setNeutral(newFeedback);
+  //   const newTotal = good + newFeedback + bad;
+  //   setTotal(newTotal);
+  //   const newAverage = (1 * good + 0 * newFeedback + -1 * bad) / newTotal;
+  //   setAverage(newAverage);
+  //   const newPositive = (good * 100) / newTotal;
+  //   setPositive(newPositive);
+  // };
 
-  const handleClickBad = () => {
-    const newFeedback = bad + 1;
-    setBad(newFeedback);
-    const newTotal = good + neutral + newFeedback;
-    setTotal(newTotal);
-    const newAverage = (1 * good + 0 * neutral + -1 * newFeedback) / newTotal;
-    setAverage(newAverage);
-    const newPositive = (good * 100) / newTotal;
-    setPositive(newPositive);
-  };
+  // const handleClickBad = () => {
+  //   const newFeedback = bad + 1;
+  //   setBad(newFeedback);
+  //   const newTotal = good + neutral + newFeedback;
+  //   setTotal(newTotal);
+  //   const newAverage = (1 * good + 0 * neutral + -1 * newFeedback) / newTotal;
+  //   setAverage(newAverage);
+  //   const newPositive = (good * 100) / newTotal;
+  //   setPositive(newPositive);
+  // };
 
   return (
     <div>
-      <h2>Give feedback</h2>
+      <Anecdotes />
+      {/* <h2>Give feedback</h2>
       <Button onClick={handleClickGood} text="good" />
       <Button onClick={handleClickNeutral} text="neutral" />
       <Button onClick={handleClickBad} text="bad" />
@@ -57,7 +59,7 @@ const App = () => {
         total={total}
         average={average}
         positive={positive}
-      />
+      /> */}
     </div>
   );
 };
