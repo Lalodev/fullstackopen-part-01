6c353115a0833329aeebc8a2f19d24134111a8d8
lalodev <laloing87@gmail.com>
2024-10-01 22:57:02 -0600

    1.9: unicafe paso 4

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -5,12 +5,18 @@ export const Statistics = (props) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <Display feedback={`good: ${props.good}`} />
-      <Display feedback={`neutral: ${props.neutral}`} />
-      <Display feedback={`bad: ${props.bad}`} />
-      <Display feedback={`all: ${props.total}`} />
-      <Display feedback={`average: ${props.average}`} />
-      <Display feedback={`positive: ${props.positive}%`} />
+      {props.total < 1 ? (
+        <div> No feedback given</div>
+      ) : (
+        <>
+          <Display feedback={`good: ${props.good}`} />
+          <Display feedback={`neutral: ${props.neutral}`} />
+          <Display feedback={`bad: ${props.bad}`} />
+          <Display feedback={`all: ${props.total}`} />
+          <Display feedback={`average: ${props.average}`} />
+          <Display feedback={`positive: ${props.positive}%`} />
+        </>
+      )}
     </div>
   );
 };
